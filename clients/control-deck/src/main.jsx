3af51cf7a3609bebc53d989fff3abcaa923c2039
ByteMaster2003 <viveksahani39266@gmail.com>
2025-04-16 00:26:37 +0530

    feat(control-deck): Add routing and implement protected routes
    Add /unauthorized route for displaying Unauthorized error message
    Add page for not found routes
    Add example for protected and admin routes

--- clients/control-deck/src/main.jsx
+++ clients/control-deck/src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { AuthProvider } from "ui/providers/auth-provider.jsx";
+import { ThemeProvider } from "ui/providers/theme-provider.jsx";
 
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ clients/control-deck/src/pages/admin.jsx
@@ -0,0 +1,8 @@
+export const Admin = () => {
+  return (
+    <>
+      <h1>Welcome Mr Admin</h1>
+      <h2>Your Admin route is working fine.</h2>
+    </>
+  );
+};
