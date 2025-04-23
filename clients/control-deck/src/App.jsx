ed7107c7eaa82ff569dd1ee2b6f9d3ba88f69102
ByteMaster2003 <viveksahani39266@gmail.com>
2025-04-23 11:08:27 +0530

    feat(control-deck): Add callback, login pages and udpate admin.jsx
    Add /oauth/callback route in Routes in App.jsx
    Create Login component to redirect user to login page
    Add .env.example file with example env variables
    Update index.html file to keep default page in dark colour

--- clients/control-deck/src/App.jsx
+++ clients/control-deck/src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFound, RoleProtectedRoutes, Unauthorized } from "ui/components";
 
 import { Admin } from "./pages/admin.jsx";
+import { AuthCallback } from "./pages/callback.jsx";
 import { Home } from "./pages/home.jsx";
+import { Login } from "./pages/login.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/unauthorized" element={<Unauthorized />} />
+        <Route path="/oauth/callback" element={<AuthCallback />} />
 
-        <Route element={<RoleProtectedRoutes allowedRoles={["Admin"]} />}>
+        <Route element={<RoleProtectedRoutes allowedRoles="Admin,User" />}>
           <Route path="/admin" element={<Admin />} />
         </Route>
 
--- clients/control-deck/src/pages/admin.jsx
+++ clients/control-deck/src/pages/admin.jsx
@@ -1,8 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
 export const Admin = () => {
   return (
-    <>
-      <h1>Welcome Mr Admin</h1>
-      <h2>Your Admin route is working fine.</h2>
-    </>
+    <Box>
+      <Typography>Hello world</Typography>
+    </Box>
   );
 };
